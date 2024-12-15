32c3df26d58068f01bc43e86867ba7e27d12ccee
leehendrick <imhendrick16@gmail.com>
2024-12-15 16:54:39 -0500

    update vite conf

--- vite.config.js
+++ vite.config.js
@@ -8,20 +8,8 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
+        vue(),
     ],
 
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
-        },
-    },
 
 });
