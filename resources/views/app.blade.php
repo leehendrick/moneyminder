cd90d85e1df50c6b58eac2cfc6f56583ef805a64
leehendrick <imhendrick16@gmail.com>
2024-12-17 23:14:59 -0500

    update blade page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-50">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>MoneyMinder</title>
     @vite('resources/css/app.css')
 </head>
-<body>
+<body class="h-full">
 <div id="app"></div>
     @vite('resources/js/app.js')
 </body>
